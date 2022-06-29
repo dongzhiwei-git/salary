a3699beacf4e946734a70be1827ac3222ebe9b7c
dongzhiwei <2660847388@qq.com>
2022-06-29 23:55:38 +0800

    001

--- script/mysql.sql
+++ script/mysql.sql
@@ -121,7 +121,11 @@ CREATE TABLE `code`
     `id`   bigint(20) unsigned NOT NULL AUTO_INCREMENT,
     `code` varchar(20)         NOT NULL DEFAULT '' COMMENT '验证码',
     PRIMARY KEY (`id`)
+
+
+
 ) ENGINE = InnoDB
+
     
 
 
